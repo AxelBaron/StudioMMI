c5d14235f6a49f2e840fdcc9bd5e7a1799f743f3
skytoa <damien@gmail.com>
2016-03-30 11:02:01 +0200

    Config DB.php

---
+++ backoffice/config/db.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: skytoa
+ * Date: 30/03/2016
+ * Time: 09:47
+ */
+
+define("DB_HOST", "localhost:8888");
+define("DB_NAME", "studio");
+define("DB_USER", "root");
+define("DB_PASS", "root");
