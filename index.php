4161a1a62c4994f7fa7e3493bc16f2cdd6a9cc85
Macussin <maxime.gatouillat@gmail.com>
2016-03-31 15:59:35 +0200

    header

--- index.php
+++ index.php
@@ -1,43 +1,51 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-  <meta charset="UTF-8">
-  <title>Studio MMI</title>
-  <link rel="stylesheet" href="css/global.css">
+	<meta charset="UTF-8">
+	<title>Studio MMI</title>
+	<link rel="stylesheet" href="css/global.css">
 </head>
+
 <body>
 
+	<section id="header">
+		<article>
+			<img class="LogoMMI" src="img/logo-blanc.svg" alt="Logo Studio MMI">
+			<img class="ScrollDown" src="img/scroll-down.png" alt="Défilez vers le bas">
+		</article>
+	</section>
 
+	<section id='contact'>
+		<article>
+			<form>
+				<input type="text" placeholder="Nom" name="nom" value="">
+				<input type="text" placeholder="Prenom" name="prenom" value="">
+				<input type="text" placeholder="Societe" name="societe" value="">
+				<input type="text" placeholder="Mail" name="mail" value="">
+				<textarea placeholder="message" name="message" value=""></textarea>
+				<input type="submit" name="name" value="Envoyer">
+			</form>
+			<div>
+				<h2>Studio MMI</h2>
+				<p>
+					9 rue de Québec
+					<br /> 10430 Rosières-près-Troyes
+					<br /> contact@studio3412.fr
+					<br /> 0345768712
+				</p>
+			</div>
+		</article>
+	</section>
 
-  <section id='contact'>
-    <article>
-      <form>
-        <input type="text" placeholder="Nom" name="nom" value="">
-        <input type="text" placeholder="Prenom" name="prenom" value="">
-        <input type="text" placeholder="Societe" name="societe" value="">
-        <input type="text" placeholder="Mail" name="mail" value="">
-        <textarea placeholder="message" name="message" value=""></textarea>
-        <input type="submit" name="name" value="Envoyer">
-      </form>
-      <div>
-        <h2>Studio MMI</h2>
-        <p>
-          9 rue de Québec<br />
-          10430 Rosières-près-Troyes <br />
-          contact@studio3412.fr<br />
-          0345768712
-        </p>
-      </div>
-    </article>
-  </section>
-
-  <footer>
-    <div>
-      <img src="img/iut.jpg" alt="iut">
-      <img src="img/canal32.jpeg" alt="canal32">
-      <img src="img/tpz.png" alt="tpz">
-    </div>
-    <p><a href="troyespointzero.fr">© Troyes Point Zero</a> - Tout droits réservés.</p>
-  </footer>
+	<footer>
+		<div>
+			<img src="img/iut.jpg" alt="iut">
+			<img src="img/canal32.jpeg" alt="canal32">
+			<img src="img/tpz.png" alt="tpz">
+		</div>
+		<p><a href="troyespointzero.fr">© Troyes Point Zero</a> - Tout droits réservés.</p>
+	</footer>
 </body>
+
 </html>
