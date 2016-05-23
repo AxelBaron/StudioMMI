a55dc79189440330c922b476261f635ef76f59d4
Macussin <maxime.gatouillat@gmail.com>
2016-05-23 16:18:25 +0200

    page materiel & footer

---
+++ materiel.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="UTF-8">
+	<title>Studio MMI</title>
+	<link rel="stylesheet" href="css/global.css">
+</head>
+
+<body>
+	<section id="menu">
+		<article class="img">
+          <a href="index.php"><img src="img/logo-vert.svg" alt="Logo Studio MMI"></a>
+		</article>	
+		<article class="menu">
+			<nav>
+				<ul>
+					<li><a href="index.php#presentation" title="Studio">Studio</a></li>
+					<li><a href="index.php#materiel" title="Matériel">Matériel</a></li>
+					<li><a href="index.php#contact" title="Contact">Contact</a></li>
+					<li class="picto">
+						<img src="img/connexion.svg" alt="pictogramme connexion">
+						<a href="#" title="Connexion">Connexion</a>
+					</li>
+				</ul>
+			</nav>
+		</article>
+		
+	</section>
+	    
+	<section id="liste-materiel">
+	    <h2>MATERIEL</h2>
+	    <p>Camera Blackmagic 4K</p>
+	    <p>Camera Blackmagic 4K</p>
+	    <p>Camera Blackmagic 4K</p>
+	    <p>Camera Blackmagic 4K</p>
+        <h4>Liste non exhaustive, d'autres matériels peuvent être loués sur demande.</h4>
+	</section>
+
+	<footer id="footer-commun">
+		<p><a href="troyespointzero.fr">© Troyes Point Zero</a> - Tout droits réservés.</p>
+	</footer>
+</body>
+
+</html>
